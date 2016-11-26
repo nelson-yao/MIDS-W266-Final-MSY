9ca1b8bffc8d5b26ae23772d446e05d7414c83fa
WillahScott <willie9monge@gmail.com>
2016-11-26 12:34:38 -0500

    Updated `run_parse.py` to perform BOW extraction

--- parsing/run_parse.py
+++ parsing/run_parse.py
@@ -1,33 +1,36 @@
 ## PARSE ALL FILES
 # Unzip + Parse all the .gz files on the specified folder
 #
+# Usage: python run_parse.py path/to/inputfolder/ path/to/outputfolder/
+#
 
 import parsing_tools as prs
 import sys
+import os
 
 def main():
 
-	# Check if a path was specified
-	if len(sys.argv) > 1:
-		ZIP_FOLDER_PATH = sys.argv[1]
-	else:
-		ZIP_FOLDER_PATH = '../data/8k-gz/'
+	# Check if the input/output paths were specified
+	if len(sys.argv) < 3:
+		print('\n\033[31mERROR:\nPlease specify a input folder and an output folder...\033[0m\n')
+		print('\n\033[35m\nExample of usage: python run_parse.py path/to/inputfolder/ path/to/outputfolder/\033[0m\n')
+		sys.exit()
 
-	BASE_FOLDER_PATH = '/'.join( list(filter(lambda x: x, ZIP_FOLDER_PATH.split('/')))[:-1] ) + '/'
+	input_folder = sys.argv[1]
+	output_folder = sys.argv[2]
 
-	UNZIPPED_FOLDER = '{}unzipped/'.format(BASE_FOLDER_PATH)
-	PARSED_FOLDER = '{}parsed/'.format(BASE_FOLDER_PATH)
+	# Check if the given folders exist
+	if not os.path.isdir(input_folder):
+		print( '\n\033[31mERROR:\nInput folder {} not found\033[0m\n'.format(input_folder) )
+		sys.exit()
+	if not os.path.isdir(output_folder):
+		print( '\n\033[31mERROR:\Output folder {} not found\033[0m\n'.format(output_folder) )
+		sys.exit()
 
 
-	# First unzip all files
-	prs.unzip_files(ZIP_FOLDER_PATH, output_folder=UNZIPPED_FOLDER )
-
-	# Second parse unzipped folder
-	prs.parse_8kfolder(UNZIPPED_FOLDER, output_folder=PARSED_FOLDER)
+	prs.parse_files(folder=input_folder, output_folder=output_folder)
 
 
 
 if __name__ == '__main__':
 	main()
-
-
