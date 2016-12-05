5207e3e37fd57c5c7ebba7e4539bd533c84394d0
judofighter25 <maxshenla@gmail.com>
2016-12-04 19:02:07 -0800

    max update

---
+++ Basic Model/CNN/data_processor.py
@@ -0,0 +1,29 @@
+import nltk.data
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+def load_features(input_data):
+
+    return_data = []
+    for i in range(input_data.shape[0]):
+        return_data.append(tokenizer.tokenize(input_data.iloc[i]))
+    
+    return (return_data)
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Create a batch iterator.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+    yield shuffled_data[start_index:end_index]
