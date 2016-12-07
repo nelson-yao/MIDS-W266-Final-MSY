3eb156d5bcd9ee23cb848fc713a9ca803382fec4
judofighter25 <maxshenla@gmail.com>
2016-12-07 00:50:00 -0800

    Update Data processor

--- Basic Model/CNN/data_processor.py
+++ Basic Model/CNN/data_processor.py
@@ -1,19 +1,45 @@
 import nltk.data
 import numpy as np
+import re
 from nltk import word_tokenize
+from nltk.corpus import stopwords
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-def load_features(input_data):
-
+def load_features(input_data, doc_n=200):
+    
     return_data = []
     for i in range(input_data.shape[0]):
         tokens = word_tokenize(input_data.iloc[i])
-        out = ' '.join(word for word in tokens[:200])
+        tokens = list(set(tokens) - set(stopwords.words('english')))
         
+        out = ' '.join(preprocessor(word) for word in tokens[:doc_n])
         return_data.append(out)
         #return_data.append(tokenizer.tokenize(input_data.iloc[i]))
     
     return (return_data) #return_data)
+
+def preprocessor(s):
+    """
+    """
+    s=s.lower().strip()
+    s=re.sub("[,.!?:;/~*]"," ",s)
+    #remove duplicated 0s and 1s
+    s=re.sub("[0-9]*","",s)
+    #Number longer than 5 digit
+    s=re.sub("[0-9]{5,}","",s)
+    #stem end with 'ly'
+    s=re.sub("ly\s"," ",s)
+    #remove plural
+    s=re.sub("s\s"," ",s)
+    s=re.sub("s\Z"," ",s)
+    #remove _ as start of the word
+    s=re.sub("\s[_]+"," ",s)
+    #remove stem end with 'ness'
+    s=re.sub("ness\s"," ",s)
+    s=re.sub("ing\s"," ",s)
+
+    return s
+
 
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
     """
