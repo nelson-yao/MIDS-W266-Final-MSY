ad7d8646c286c37d3d11435e926b9895fa7c5ae3
judofighter25 <maxshenla@gmail.com>
2016-12-18 15:40:28 -0800

    update file

--- models/CNN/cnn_net.py
+++ models/CNN/cnn_net.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class CNN(object):
     """
-
+    Convolution Neural Net with word embedding for Text Classification
     """
     def __init__(
       self, sequence_length, num_classes, vocab_size,
@@ -15,18 +15,21 @@ class CNN(object):
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
-        # Keeping track of l2 regularization loss (optional)
+        self.input_suprise = tf.placeholder(tf.float32, [None, 1], name="input_surprise")
+
+        # L2 loss function
         l2_loss = tf.constant(0.0)
 
         # Embedding layer
-        with tf.device('/cpu:0'), tf.name_scope("embedding"):
+        #with tf.device('/cpu:0'), tf.name_scope("embedding"):
+        with tf.name_scope("embedding"):
             W = tf.Variable(
                 tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
                 name="W")
             self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
-        # Create a convolution + maxpool layer for each filter size
+        # Convolution + maxpool layer give each filter
         pooled_outputs = []
         for i, filter_size in enumerate(filter_sizes):
             with tf.name_scope("conv-maxpool-%s" % filter_size):
@@ -37,30 +40,34 @@ class CNN(object):
                 conv = tf.nn.conv2d(
                     self.embedded_chars_expanded,
                     W,
-                    strides=[1, 1, 1, 1],
+                    strides=[1, 1, 1, 1],        #strides
                     padding="VALID",
                     name="conv")
-                # Apply nonlinearity
-                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                # Maxpooling over the outputs
+                
+                # Relu
+                relu = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                
+                #  Standard maxpool
                 pooled = tf.nn.max_pool(
-                    h,
+                    relu,
                     ksize=[1, sequence_length - filter_size + 1, 1, 1],
                     strides=[1, 1, 1, 1],
                     padding='VALID',
                     name="pool")
                 pooled_outputs.append(pooled)
 
-        # Combine all the pooled features
-        num_filters_total = num_filters * len(filter_sizes)
-        self.h_pool = tf.concat(3, pooled_outputs)
-        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
-        # Add dropout
+        num_filters_total = num_filters * len(filter_sizes) + 1
+        self.h_pool = tf.concat(3, pooled_outputs)
+        #features = tf.concat(1, [self.input_suprise, self.h_pool])
+        #Concatnate  earning suprise with  word embedding
+        self.h_pool_flat = tf.concat(1, [tf.reshape(self.h_pool, [-1, num_filters_total -1]), self.input_suprise])
+
+        # Dropout
         with tf.name_scope("dropout"):
             self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
 
-        # Final (unnormalized) scores and predictions
+        # Initial prediction
         with tf.name_scope("output"):
             W = tf.get_variable(
                 "W",
@@ -69,15 +76,17 @@ class CNN(object):
             b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
             l2_loss += tf.nn.l2_loss(W)
             l2_loss += tf.nn.l2_loss(b)
-            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
-            self.predictions = tf.argmax(self.scores, 1, name="predictions")
 
-        # CalculateMean cross-entropy loss
+            self.xw_out = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.xw_out, 1, name="predictions")
+
+        # Loss Function
         with tf.name_scope("loss"):
-            losses = tf.nn.softmax_cross_entropy_with_logits(self.scores, self.input_y)
+            losses = tf.nn.softmax_cross_entropy_with_logits(self.xw_out, self.input_y)
             self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
-        # Accuracy
+            
+        # Calc Accuracy
         with tf.name_scope("accuracy"):
             correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
             self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
--- models/CNN/data_processor.py
+++ models/CNN/data_processor.py
@@ -9,42 +9,68 @@ def load_features(input_data, doc_n=200):
     
     return_data = []
     for i in range(input_data.shape[0]):
-        tokens = word_tokenize(input_data.iloc[i])
-        tokens = list(set(tokens) - set(stopwords.words('english')))
+        #tokens = word_tokenize(input_data.iloc[i])
+        #tokens = list(set(tokens) - set(stopwords.words('english')))
+        #tokens = filter(lambda x: x not in stopwords.words('english'), paragraph.split())
         
-        out = ' '.join(preprocessor(word) for word in tokens[:doc_n])
+        paragraph = input_data.iloc[i].replace('TEXT:', '').replace('Table of Contents', '')\
+        .replace("[,.!?:;/~*]", '').replace('Pre-commencement communications pursuant to Rule','')\
+        .replace("Exhibits",'').replace("Securities Exchange Act of 1934",'').replace("Securities Act of 1933",'')\
+        .replace("Exchange Act",'').replace("8-K",'').replace("1933",'').replace("1934",'')
+        tokens = list(set(paragraph.lower().split()) - set(stopwords.words('english')))
+        
+    #start_index = 20
+    #end_index = min(start_index + doc_n, len(tokens))     
+        out = ''
+        for i, word in enumerate(tokens):
+            word = preprocessor(word)
+            if word != '' and i > 0:
+                out = ' '.join([out, word])
+            elif word != '' and i == 0:
+                out = ''.join([out, word])
+        
         return_data.append(out)
+        
+        #out = ' '.join(preprocessor(word) for word in tokens[start_index:end_index])
         #return_data.append(tokenizer.tokenize(input_data.iloc[i]))
-    
-    return (return_data) #return_data)
+    return (return_data)
+
 
 def preprocessor(s):
     """
+    Parsing to remove symbols, white space, and comma, etc.
     """
     s=s.lower().strip()
-    s=re.sub("[,.!?:;/~*]"," ",s)
+    s=re.sub('[!@#$:;,]', '', s)
+    s=re.sub("[,.!?:;/~*()]","",s)
+    re.sub(',','', s)
     #remove duplicated 0s and 1s
-    s=re.sub("[0-9]*","",s)
+    #s=re.sub("[0-9]*","",s)
     #Number longer than 5 digit
-    s=re.sub("[0-9]{5,}","",s)
+    #s=re.sub("[0-9]{5,}","",s)
+    #s=re.sub("\d*", '', s)
+    #s=re.sub("[0-9]{1,}","number",s)
     #stem end with 'ly'
-    s=re.sub("ly\s"," ",s)
+    s=re.sub("ly\s","",s)
     #remove plural
-    s=re.sub("s\s"," ",s)
-    s=re.sub("s\Z"," ",s)
+    #s=re.sub("s\s"," ",s)
+    #s=re.sub("s\Z"," ",s)
     #remove _ as start of the word
-    s=re.sub("\s[_]+"," ",s)
+    s=re.sub("\s[_]+","",s)
     #remove stem end with 'ness'
-    s=re.sub("ness\s"," ",s)
-    s=re.sub("ing\s"," ",s)
-
+    s=re.sub("ness\s","",s)
+    s=re.sub("[|_|]","",s)
+    s=re.sub("ing\s","",s)
+    s = re.sub('"registrant"',"",s)
+    #s=re.sub(r'[^\w]', ' ', s)
     return s
 
 
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
     """
-    Create a batch iterator.
+    Create data batches for training
     """
+    #np.random.seed(9)
     data = np.array(data)
     data_size = len(data)
     num_batches_per_epoch = int(len(data)/batch_size) + 1
@@ -60,3 +86,12 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
             start_index = batch_num * batch_size
             end_index = min((batch_num + 1) * batch_size, data_size)
             yield shuffled_data[start_index:end_index]
+
+def one_iter(data):
+    """
+    Create a batch iterator for development set
+    """
+    data = np.array(data)
+   
+    for batch_num in range(1):
+        yield data
